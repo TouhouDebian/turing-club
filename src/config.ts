1f3b3b771cc60f4cc8df547ba3131534ac3f4dd4
THDeb <194379267+TouhouDebian@users.noreply.github.com>
2026-06-29 15:12:47 +0800

    I use arch linux btw

--- src/config.ts
+++ src/config.ts
@@ -16,13 +16,13 @@ export const siteConfig: SiteConfig = {
 		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
-		enable: false,
-		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		enable: true,
+		src: "/banner/89596288_p0.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
-			enable: false, // Display the credit text of the banner image
-			text: "", // Credit text to be displayed
-			url: "", // (Optional) URL link to the original artwork or artist's page
+			enable: true, // Display the credit text of the banner image
+			text: "顶部图片出处 / Header image source", // Credit text to be displayed
+			url: "https://www.pixiv.net/artworks/89596288", // (Optional) URL link to the original artwork or artist's page
 		},
 	},
 	toc: {
