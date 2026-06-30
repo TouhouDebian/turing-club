55348e0e58043f75b351c23514821f3d842bce27
THDeb <194379267+TouhouDebian@users.noreply.github.com>
2026-06-30 09:37:53 +0800

    添加留言功能，并修复若干bug

--- src/config.ts
+++ src/config.ts
@@ -42,6 +42,14 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Archive,
 		LinkPreset.About,
 		{
+			name: "友链",
+			url: "/links/",
+		},
+		{
+			name: "留言",
+			url: "/comment/",
+		},
+		{
 			name: "GitHub",
 			url: "https://github.com/TouhouDebian/turing-club", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
